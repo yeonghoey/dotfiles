7aab19b8682c3f2aa7c3574e245214472b700d21
Yeongho Kim <yeonghoey@gmail.com>
2016-03-24 22:57:38 +0900

    Remove extaneous features

--- Brewfile
+++ Brewfile
@@ -3,6 +3,9 @@ cask_args appdir: '/Applications'
 tap 'caskroom/cask'
 tap 'homebrew/bundle'
 
+brew 'tmux'
+brew 'hub'
+
 cask '1password'
 cask 'google-chrome'
 cask 'slack'
--- vim/install.sh
+++ vim/install.sh
@@ -4,7 +4,7 @@ VUNDLE_REPO=https://github.com/VundleVim/Vundle.vim.git
 VUNDLE_PATH=~/.vim/bundle/Vundle.vim
 VUNDLE_FILE=~/.vundles
 
-if [[ ! -a $VUNDLEPATH ]]
+if [[ ! -a $VUNDLE_PATH ]]
 then
     git clone $VUNDLE_REPO $VUNDLE_PATH
 fi
