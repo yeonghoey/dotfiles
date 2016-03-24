3bf0a05e4669391817e96d265bf669e3bfc7cdae
Yeongho Kim <yeonghoey@gmail.com>
2016-03-24 22:35:00 +0900

    Fix zsh-autosuggetion installation

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,14 +5,15 @@ chsh -s /bin/zsh
 
 OHMYZSH_REPO=git://github.com/robbyrussell/oh-my-zsh.git
 
-if [[ ! -a $OHMYZSH_PATH ]]
+if [[ ! -a $ZSH ]]
 then
     git clone $OHMYZSH_REPO $ZSH
 fi
 
 AUTOSUGGESTIONS_REPO=git://github.com/zsh-users/zsh-autosuggestions
 AUTOSUGGESTIONS_PATH=$ZSH_CUSTOM/plugins/zsh-autosuggestions
-if [[ ! -a $AUTOSUGGESTIONS_REPO ]]
+
+if [[ ! -a $AUTOSUGGESTIONS_PATH ]]
 then
     git clone $AUTOSUGGESTIONS_REPO $AUTOSUGGESTIONS_PATH
 fi
