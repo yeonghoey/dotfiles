d71a6e0ba7610db3ef6c08e87f4bb3d40f905c27
Yeongho Kim <yeonghoey@gmail.com>
2016-03-27 13:37:33 +0900

    Add python install

---
+++ python/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if test ! $(which virtualenv)
+then
+  sudo pip install virtualenv
+fi
+
+PYTHON_PATH=$DOTFILES_ROOT/python
+
+if [[ ! -a $PYTHON_PATH/venv ]]
+then
+  virtualenv $PYTHON_PATH/venv
+  source $PYTHON_PATH/venv/bin/activate
+  pip install --no-cache-dir -r $PYTHON_PATH/requirements.txt
+  deactivate
+fi
