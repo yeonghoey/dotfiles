ba70a1f8d7cb6596bf43c564a8d8a163be9ddaa9
Yeongho Kim <yeonghoey@gmail.com>
2016-04-09 16:20:59 +0900

    Make Finder show hidden files

--- osx/install.sh
+++ osx/install.sh
@@ -5,3 +5,4 @@ sudo softwareupdate -i -a
 
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+defaults write com.apple.finder AppleShowAllFiles YES
