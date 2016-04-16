55c7ff65c3a61a0006d5d9377446d8abfbb475cc
Yeongho Kim <yeonghoey@gmail.com>
2016-04-17 06:58:00 +0900

    Add zsh-syntax-highlighting

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -2,7 +2,8 @@ export ZSH=$HOME/.oh-my-zsh
 ZSH_CUSTOM="$ZSH/custom"
 
 ZSH_THEME='afowler'
-plugins=(git colorize zsh-autosuggestions)
+plugins=(git colorize zsh-autosuggestions \
+    zsh-syntax-highlighting) # syntax-highlighting should be at last
 
 # ----------------------------------------------------------------------------
 OHMYZSH_REPO=git://github.com/robbyrussell/oh-my-zsh.git
@@ -17,6 +18,14 @@ if [[ ! -a $AUTOSUGGESTIONS_PATH ]]
 then
     git clone $AUTOSUGGESTIONS_REPO $AUTOSUGGESTIONS_PATH
 fi
+
+SYNTAXHIGHLIGHTING_REPO=git@github.com:zsh-users/zsh-syntax-highlighting.git
+SYNTAXHIGHLIGHTING_PATH=$ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+if [[ ! -a $SYNTAXHIGHLIGHTING_PATH ]]
+then
+    git clone $SYNTAXHIGHLIGHTING_REPO $SYNTAXHIGHLIGHTING_PATH
+fi
+
 source $ZSH/oh-my-zsh.sh
 # ----------------------------------------------------------------------------
 
