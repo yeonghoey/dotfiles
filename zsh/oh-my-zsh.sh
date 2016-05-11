f0b5fa1e2c0f0683765d1d7940b4a053120f67d3
Yeongho Kim <yeonghoey@gmail.com>
2016-05-12 08:05:29 +0900

    Add oh-my-zsh osx plugin

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -2,7 +2,7 @@ export ZSH=$HOME/.oh-my-zsh
 ZSH_CUSTOM="$ZSH/custom"
 
 ZSH_THEME='afowler'
-plugins=(git colorize zsh-autosuggestions \
+plugins=(git colorize zsh-autosuggestions osx \
     zsh-syntax-highlighting) # syntax-highlighting should be at last
 
 # ----------------------------------------------------------------------------
