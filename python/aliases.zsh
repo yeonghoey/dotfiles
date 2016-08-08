ecc64d30aaf6bdcd25f20482f270cee396a2d3e6
yeonghoey <yeonghoey@gmail.com>
2016-08-08 16:18:53 +0900

    Add rmv for virutalenvwrapper

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,4 @@
 # shortcut for making and switch virtualenv
 alias v='workon $(basename $(git rev-parse --show-toplevel || echo "."))'
 alias mkv='mkvirtualenv $(basename $(git rev-parse --show-toplevel || echo "."))'
+alias rmv='rmvirtualenv $(basename $(git rev-parse --show-toplevel || echo "."))'
