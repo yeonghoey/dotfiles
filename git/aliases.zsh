2eaadef2e4e2520167182a6da94b65e4f232e7b0
yeonghoey <yeonghoey@gmail.com>
2016-09-05 16:16:35 +0900

    More git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,1 +1,4 @@
-alias rebaseom="git fetch origin master && git rebase -i --autosquash origin/master"
+alias grom="git fetch origin master && git rebase -i --autosquash origin/master"
+alias grod="git fetch origin develop  && git rebase -i --autosquash origin/develop"
+alias grum="git fetch upstream master && git rebase -i --autosquash upstream/master"
+alias grud="git fetch upstream develop  && git rebase -i --autosquash upstream/develop"
