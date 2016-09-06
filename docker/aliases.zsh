406598eba7727c8a8ac64e2e4ccf27899346c6c6
yeonghoey <yeonghoey@gmail.com>
2016-09-06 09:14:45 +0900

    Add docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,5 @@
-alias dockerinit='eval $(docker-machine env default)'
+alias docker-init='eval $(docker-machine env default)'
 alias jupyter-scala='docker run --rm -it -p 8888:8888 dockoey/jupyter-scala:latest'
+
+alias docker-rm-stopped='docker rm $(docker ps -a -q)'
+alias docker-rmi-none=$'docker rmi $(docker images | grep "^<none>" | awk \'{print $3}\')'
