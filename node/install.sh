df49ca11c88800ceb85cc39d3156904dfa1fa945
yeonghoey <yeonghoey@gmail.com>
2016-10-09 02:21:07 +0900

    Add vmd

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-if test ! $(which 'git-open')
-then
+if test ! $(which 'git-open'); then
   sudo npm install -g git-open
+fi
+
+if test ! $(which 'vmd'); then
+  sudo npm install -g vmd
 fi
