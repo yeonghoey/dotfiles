f8168191d924d465260ac09a4c983cbef4760fdb
yeonghoey <yeonghoey@gmail.com>
2016-12-19 17:21:52 +0900

    Upgrade ec2ip

--- python/ec2ip.py
+++ python/ec2ip.py
@@ -5,17 +5,13 @@ import boto3
 
 def main(name):
     ec2 = boto3.resource('ec2')
-    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name',
-                                               'Values': ['running']}])
+    instances = ec2.instances.filter(Filters=[
+        {'Name': 'instance-state-name', 'Values': ['running']},
+        {'Name': 'tag:Name', 'Values': [name]}
+    ])
     for instance in instances:
-        if not instance.tags:
-            continue
-        for tag in instance.tags:
-            if tag['Key'] == 'Name' and tag['Value'] == name:
-                print(instance.private_ip_address)
-                return 0
-    print('Not found', file=sys.stderr)
-    return 1
+        print(instance.private_ip_address)
+
 
 if __name__ == '__main__':
     main(sys.argv[1])
