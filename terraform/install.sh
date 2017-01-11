65d564b0e767ba00c0125b000f994428a492c137
yeonghoey <yeonghoey@gmail.com>
2017-01-11 16:27:48 +0900

    Add terraform/install.sh

---
+++ terraform/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+target_dir="$HOME/bin"
+mkdir -p "$target_dir"
+
+case "$(uname -s)" in
+    Darwin)
+        download_url='https://releases.hashicorp.com/terraform/0.8.3/terraform_0.8.3_darwin_amd64.zip'
+        ;;
+    Linux)
+        download_url='https://releases.hashicorp.com/terraform/0.8.3/terraform_0.8.3_linux_amd64.zip'
+        ;;
+    *)
+        echo 'Unsupported OS'
+        echo 'Download binary from https://www.terraform.io/downloads.html manually'
+        exit 1
+esac
+
+zipfile="/tmp/terraform.zip"
+wget -O "$zipfile" "$download_url"
+unzip "$zipfile" -d "$target_dir"
