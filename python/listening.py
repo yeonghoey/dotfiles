9285e16b2345fe7cc1185f6cd7e1ebfb647d96ce
Yeongho Kim <yeonghoey@gmail.com>
2017-03-20 08:45:38 +0900

    Refactor listening

--- python/listening.py
+++ python/listening.py
@@ -8,33 +8,34 @@ import time
 import clipboard
 
 
-def main(ankidir, inpath):
+def main(ankidir, inpath, ss, to):
     if not os.path.exists(inpath):
         print('"%s" not exsisting' % inpath, file=sys.stderr)
         exit(1)
-    while True:
-        line = raw_input('listening> ')
-        line = line.strip()
-        try:
-            ss, to = line.split()
-        except ValueError:
-            print('invalid section: %s' % line, file=sys.stderr)
-        else:
-            outname = make_outname()
-            outpath = os.path.join(ankidir, outname)
-            extract(inpath, outpath, ss, to)
-            clipboard.copy('[sound:%s]' % outname)
-            play(outpath)
+    outname = make_outname(inpath)
+    outpath = os.path.join(ankidir, outname)
+    extract(inpath, outpath, ss, to)
+    copy_clipboard(outname)
+    play(outpath)
 
 
-def make_outname():
-    return 'listening-%d.mp3' % time.time()
+def make_outname(inpath):
+    _, tail = os.path.split(inpath)
+    name, _ = os.path.splitext(tail)
+    return '%s-%d.mp3' % (name, time.time())
 
 
 def extract(inpath, outpath, ss, to):
-    cmd = 'ffmpeg -y -i "{inpath}" -vn -codec:a libmp3lame -qscale:a 3 '\
+    cmd = 'ffmpeg -y -loglevel 0 '\
+          '-i "{inpath}" -vn -codec:a libmp3lame -qscale:a 3 '\
           '-ss {ss} -to {to} "{outpath}"'.format(**locals())
     os.system(cmd)
+
+
+def copy_clipboard(outname):
+    sound = '[sound:%s]' % outname
+    print('copy: %s' % sound)
+    clipboard.copy(sound)
 
 
 def play(outpath):
@@ -42,9 +43,14 @@ def play(outpath):
     os.system(cmd)
 
 
+def name(path):
+    return os.path.split(path)[1]
+
 if __name__ == '__main__':
     ankidir = sys.argv[1]
     inpath = sys.argv[2]
+    ss = sys.argv[3]
+    to = sys.argv[4]
     print('ankidir: %s' % ankidir)
-    print(' inpath: %s' % inpath)
-    main(ankidir, inpath)
+    print('extract: %s (%s ~ %s)' % (inpath, ss, to))
+    main(ankidir, inpath, ss, to)
