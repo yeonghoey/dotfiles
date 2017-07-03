83f5913b4949cea960506d4a9e7fa9954e008680
Yeongho Kim <yeonghoey@gmail.com>
2017-07-03 15:03:40 +0900

    Delete virtualenvwrapper aliases

--- python/aliases.zsh
+++
@@ -1,4 +0,0 @@
-# shortcut for making and switch virtualenv
-alias v='workon $(basename $(git rev-parse --show-toplevel || echo "."))'
-alias mkv='mkvirtualenv $(basename $(git rev-parse --show-toplevel || echo "."))'
-alias rmv='rmvirtualenv $(basename $(git rev-parse --show-toplevel || echo "."))'
