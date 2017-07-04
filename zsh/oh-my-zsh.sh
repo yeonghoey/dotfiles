e32d330f8fc32d2d4050aa911a7d6bbdd9f63132
Yeongho Kim <yeonghoey@gmail.com>
2017-07-05 08:59:40 +0900

    Stop using syntax-highlighting

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -2,8 +2,7 @@ export ZSH=$HOME/.oh-my-zsh
 ZSH_CUSTOM="$ZSH/custom"
 
 ZSH_THEME='afowler'
-plugins=(git colorize osx zsh-navigation-tools \
-    zsh-syntax-highlighting) # syntax-highlighting should be at last
+plugins=(git colorize osx zsh-navigation-tools)
 
 # ----------------------------------------------------------------------------
 OHMYZSH_REPO=git://github.com/robbyrussell/oh-my-zsh.git
@@ -11,13 +10,5 @@ if [[ ! -a $ZSH ]]
 then
     git clone $OHMYZSH_REPO $ZSH
 fi
-
-SYNTAXHIGHLIGHTING_REPO=https://github.com/zsh-users/zsh-syntax-highlighting.git
-SYNTAXHIGHLIGHTING_PATH=$ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-if [[ ! -a $SYNTAXHIGHLIGHTING_PATH ]]
-then
-    git clone $SYNTAXHIGHLIGHTING_REPO $SYNTAXHIGHLIGHTING_PATH
-fi
-
 source $ZSH/oh-my-zsh.sh
 # ----------------------------------------------------------------------------
